43174127c99d45aa4cdd4d490687b814ccc0c3c5
Norbert Borek <norbertborek92@gmail.com>
2022-10-09 13:53:16 +0200

    Done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        String temp = Integer.toString(number);
+        int[] numbers = new int[temp.length()];
+        for (int i = 0; i < temp.length(); i++) {
+            numbers[i] = temp.charAt(i) - '0';
+        }
+        int temp2 = 0;
+        for (int i = numbers.length -1; i >= 0; i--){
+            temp2 += numbers[i];
+        }
+        System.out.println(temp2);
     }
 }
